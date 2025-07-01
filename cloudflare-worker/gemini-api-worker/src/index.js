063d6fb7153948926c0a1e4900125fbec5afc334
Faisal Javid <faisalljavid@gmail.com>
2025-07-01 19:01:28 +0530

    Enhance Gemini API Worker: introduce configuration for AI model responses, including a new system instruction for stock performance reporting, and update response structure to return a JSON object with the reply.

--- cloudflare-worker/gemini-api-worker/src/index.js
+++ cloudflare-worker/gemini-api-worker/src/index.js
@@ -17,14 +17,42 @@ export default {
 			apiKey: env.GEMINI_API_KEY
 		})
 
+		const config = {
+			temperature: 1.1,
+			thinkingConfig: {
+				thinkingBudget: 0,
+			},
+			systemInstruction: [
+				{
+					text: `You are a trading guru. Given data on share prices over the past 3 days, write a report of no more than 150 words describing the stocks performance and recommending whether to buy, hold or sell.`
+				}
+			],
+		}
+
 		try {
+			const model = 'gemini-2.5-flash'
+			const reqBody = await request.json()
+			const userInput = reqBody.message || 'No input provided'
+
+			const contents = [
+				{
+					role: 'user',
+					parts: [
+						{
+							text: userInput
+						},
+					],
+				},
+			]
 			const response = await ai.models.generateContent({
-				model: 'gemini-2.5-flash',
-				contents: `Should I trust stock predictions from Dodgy Dave? Give a 50 word answer`
+				model,
+				config,
+				contents
 			})
 
 			const text = response.text
-			return new Response(JSON.stringify(text), { headers: corsHeaders })
+			return new Response(JSON.stringify({ reply: text }), { headers: corsHeaders })
+
 		} catch (error) {
 			console.error('Error:', error, { headers: corsHeaders })
 		}
